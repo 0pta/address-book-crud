4139d1b40c09bcdd0ac54afb1f9483294d0b3f50
Aaron Eugene Kester <aaron.opta@yahoo.com>
2017-05-07 21:19:51 -0700

    get all route, create one route, get one route and these hbs views

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,9 +3,48 @@ var router = express.Router();
 var knex = require('../db/knex');
 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET contacts listing. */
+router.get('/', (req, res, next) => {
+  knex('addresses')
+  .innerJoin('contacts', 'addresses.id', 'contacts.addresses_id')
+  .then(function(contacts) {
+    res.render('contacts/index', {
+      contacts
+    })
+  })
 });
+
+
+//Create One
+router.get('/create', (req, res)=>{
+  knex('addresses')
+  .innerJoin('contacts', 'addresses.id', 'contacts.addresses_id')
+  .then((contact)=>{
+    res.render('contacts/create', {
+      contact
+    });
+  });
+});
+
+/* GET one. */
+router.get('/:id', (req, res) => {
+  var id = req.params.id;
+  knex('contacts')
+  .innerJoin('addresses', 'addresses.id', 'contacts.addresses_id')
+  .where('contacts.id', id)
+  .first()
+  .then((contact) => {
+    res.render('contacts/contact', {
+      contact
+    })
+  })
+});
+
+/* DELETE one. */
+// router.delete('/:id', (req, res) => {
+//
+// })
+
+
 
 module.exports = router;
